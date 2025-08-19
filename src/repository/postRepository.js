5638b431f9438f2ac9cad1dea20c776d7921e016
Rameez <rameez.hameed@laderatechnology.com>
2025-08-19 18:57:24 +0530

    Sequelize to pg

---
+++ src/repository/postRepository.js
@@ -0,0 +1,17 @@
+const pool = require('../database');
+
+class PostRepository {
+    async findAll() {
+        const posts = await pool.query('SELECT * FROM "Posts" ORDER BY date DESC');
+        return posts.rows;
+    }
+
+    async create({ title, content }) {
+        const posts = await pool.query('INSERT INTO "Posts" (title, content, date) VALUES ($1, $2, NOW()) RETURNING *',
+            [title, content]
+        );
+        return posts.rows[0];
+    }
+}
+
+module.exports = new PostRepository();
